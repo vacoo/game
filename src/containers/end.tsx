fdc177a40bdb9c46c368add866767416871d80fa
mr.vacoo <sirworker@ya.ru>
2018-12-13 21:06:51 +0900

    FIX

--- src/containers/end.tsx
+++ src/containers/end.tsx
@@ -11,20 +11,24 @@ type Props = {
 };
 
 const End = class End extends React.Component<Props> {
-
     componentDidMount() {
         this.props.dispatch(GameActions.generateField());
     }
 
     result = () => {
-        const {aiScore, userScore } = this.props.game;
-        if(aiScore === userScore) {
+        const { aiScore, userScore } = this.props.game;
+
+        if (aiScore === userScore) {
             return "Вы по уровень интелекта как у ИИ";
-        } else if(aiScore < userScore) {
-            return "Вы предотваритили конец света. Поздравляю. Теперь вы герой."
+        } else if (aiScore < userScore) {
+            return "Вы предотваритили конец света. Поздравляю. Теперь вы герой.";
         } else {
             return "Мне жаль. Терминаторы захватили мир.";
         }
+    };
+
+    repeatGame = () => {
+        this.props.dispatch(GameActions.repeatGame());
     }
 
     render() {
@@ -32,7 +36,7 @@ const End = class End extends React.Component<Props> {
             <div>
                 <h1 className="title">Игра закончена</h1>
                 <p className="subtitle">{this.result()}</p>
-                <Link to="/game" replace={true} className="button" onClick={() => this.props.dispatch(GameActions.repeatGame())}>
+                <Link to="/game" replace={true} className="button" onClick={this.repeatGame}>
                     Заново
                 </Link>
             </div>
